3801a13e4eec4f049651dfc4a028dee5cdeb79f4
Mobin <mobingholizadeh10@gmail.com>
2023-02-13 17:14:58 +0330

    a logo added and some titles got changed

--- MobinShop/MobinShop/MobinShop/Controllers/HomeController.cs
+++ MobinShop/MobinShop/MobinShop/Controllers/HomeController.cs
@@ -28,5 +28,8 @@ namespace MobinShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
+
 }
